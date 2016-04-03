fe6e037ca8415819e466b32221480115771fa5d3
Mike Rivera <rivera.mj@gmail.com>
2016-04-02 23:50:05 -0400

    remove db debug

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -19,10 +19,6 @@ class Boot {
 
     if (!DB.jndiJdbcConnAvailable_?) {
 
-      println("==========================")
-      println(Props.get("db.user") + " and " + Props.get("db.password"))
-      println("==========================")
-
       val vendor = 
         new StandardDBVendor(Props.get("db.driver") openOr "org.h2.Driver",
 			     Props.get("db.url") openOr 
