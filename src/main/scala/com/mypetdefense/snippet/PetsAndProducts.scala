dbfc95c3b506bcb061c42bc1c299354bb4065bc4
Mike Rivera <rivera.mj@gmail.com>
2016-11-01 17:18:33 -0400

    add file structure for pets and products

---
+++ src/main/scala/com/mypetdefense/snippet/PetsAndProducts.scala
@@ -0,0 +1,33 @@
+package com.mypetdefense.snippet
+
+import net.liftweb.sitemap.Menu
+import net.liftweb.http.SHtml
+import net.liftweb.util.Helpers._
+import net.liftweb.common._
+import net.liftweb.util.ClearClearable
+import net.liftweb.http._
+import net.liftweb.mapper.By
+
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.time.{LocalDate, ZoneId}
+
+import com.mypetdefense.model._
+import com.mypetdefense.util.Paths._
+import com.mypetdefense.actor._
+import com.mypetdefense.util.ClearNodesIf
+import com.mypetdefense.util.SecurityContext._
+import com.mypetdefense.service.TaxJarService
+
+object PetsAndProducts extends Loggable {
+  import net.liftweb.sitemap._
+    import Loc._
+  import com.mypetdefense.util.Paths._
+
+  val menu = Menu.i("Pets and Products") / "pets-products" >>
+    loggedIn >>
+    parent
+}
+
+class PetsAndProducts extends Loggable {
+}
