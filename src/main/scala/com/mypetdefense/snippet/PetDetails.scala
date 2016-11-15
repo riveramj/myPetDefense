2c27f417fa6ac1a89765ffa975ebd4d23480484b
Mike Rivera <rivera.mj@gmail.com>
2016-11-14 23:01:28 -0500

    mobile working. broken progress bar

--- src/main/scala/com/mypetdefense/snippet/PetDetails.scala
+++ src/main/scala/com/mypetdefense/snippet/PetDetails.scala
@@ -11,7 +11,10 @@ import net.liftweb._
   import js._
   import JsCmds._
 
-import com.mypetdefense.service.PetFlowChoices
+import com.mypetdefense.service._
+  import ValidationService._
+  import PetFlowChoices._
+
 import com.mypetdefense.model._
 import java.util.Date
 import java.text.SimpleDateFormat
@@ -34,19 +37,28 @@ class PetDetails extends Loggable {
 
   val formatter = new SimpleDateFormat("MM/yy")
 
-  def proceed() = {
-    PetFlowChoices.petName(Full(petName))
-    PetFlowChoices.birthday(tryo(formatter.parse(birthday)))
-    S.redirectTo(Checkout.menu.loc.calcDefaultHref)
+  def goToCheckout() = {
+
+    val validateFields = List(
+      checkEmpty(petName, "#pet-name")
+    ).flatten
+
+    if(validateFields.isEmpty) {
+      PetFlowChoices.petName(Full(petName))
+      PetFlowChoices.birthday(tryo(formatter.parse(birthday)))
+      S.redirectTo(Checkout.menu.loc.calcDefaultHref)
+    } else {
+      validateFields.foldLeft(Noop)(_ & _)
+    }
   }
 
   def render = {
     SHtml.makeFormsAjax andThen
-    "#details-pet *" #> PetFlowChoices.petChoice.is.map(_.toString) &
-    "#details-product *" #> PetFlowChoices.petProduct.is.map(_.name.toString) &
-    "#details-size *" #> PetFlowChoices.petSize.is.map(_.toString) &
+    ".details-pet *" #> PetFlowChoices.petChoice.is.map(_.toString) &
+    ".details-product *" #> PetFlowChoices.petProduct.is.map(_.name.toString) &
+    ".details-size *" #> PetFlowChoices.petSize.is.map(_.toString) &
     "#pet-name" #> text(petName, petName = _) &
     "#birthday" #> text(birthday, birthday = _) &
-    "#to-checkout" #> SHtml.ajaxSubmit("Proceed To Checkout", proceed)
+    "#checkout" #> SHtml.ajaxSubmit("Checkout", goToCheckout)
   }
 }
