421105e542af02235905b37c0613d782d7e26329
Mike Rivera <rivera.mj@gmail.com>
2017-08-10 11:04:10 +0200

    product list fixes

--- src/main/scala/com/mypetdefense/snippet/Products.scala
+++ src/main/scala/com/mypetdefense/snippet/Products.scala
@@ -29,7 +29,28 @@ object Products extends Loggable {
 }
 
 class Products extends Loggable {
+  val prices = Price.findAll(
+    By(Price.code, "default"),
+    By(Price.active, true)
+  )
+
+  def getPriceForProduct(product: String, petType: String) = {
+    val price = prices.filter { price =>
+      val productName = price.product.obj.map(_.name.get).getOrElse("")
+      productName == s"${product} Plus for ${petType}"
+    }.headOption.map(_.price.get).getOrElse(0D)
+
+    f"$$$price%2.2f"
+  }
+
+
   def render = {
-    "#foo" #> ""
+    ".frontline-dogs .month-price *" #> getPriceForProduct("Frontline", "Dogs") &
+    ".zoguard-dogs .month-price *" #> getPriceForProduct("ZoGuard", "Dogs") &
+    ".adventure-dogs .month-price *" #> getPriceForProduct("Adventure", "Dogs") &
+    ".shieldtec-dogs .month-price *" #> getPriceForProduct("ShieldTec", "Dogs") &
+    ".frontline-cats .month-price *" #> getPriceForProduct("Frontline", "Cats") &
+    ".zoguard-cats .month-price *" #> getPriceForProduct("ZoGuard", "Cats") &
+    ".adventure-cats .month-price *" #> getPriceForProduct("Adventure", "Cats")
   }
 }
