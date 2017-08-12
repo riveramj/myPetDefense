f4f9a48ae17af56a9b2a360a4b5480fd13f9b6fd
Mike Rivera <rivera.mj@gmail.com>
2017-08-12 12:39:55 +0200

    add image file name sql

---
+++ sql-scripts/image-file-names.sql
@@ -0,0 +1,68 @@
+ select name, id, sizename from product order by id;
+
+local:
+
+update product set imagename = 'zoguard/ZoGuard-Plus-small-dog.jpg' where id = '10';
+update product set imagename = 'zoguard/ZoGuard-Plus-medium-dog.jpg' where id = '11';
+update product set imagename = 'zoguard/ZoGuard-Plus-large-dog.jpg' where id = '12';
+update product set imagename = 'zoguard/ZoGuard-Plus-xlarge-dog.jpg' where id = '13';
+
+update product set imagename = 'zoguard/ZoGuard-Plus-cat-2.jpg' where id = '26';
+update product set imagename = 'zoguard/ZoGuard-Plus-cat-2.jpg' where id = '4';
+update product set imagename = 'zoguard/ZoGuard-Plus-cat-2.jpg' where id = '5';
+
+update product set imagename = 'adventure/Adventure-Plus-small-dog.jpg' where id = '6';
+update product set imagename = 'adventure/Adventure-Plus-medium-dog.jpg' where id = '7';
+update product set imagename = 'adventure/Adventure-Plus-large-dog.jpg' where id = '8';
+update product set imagename = 'adventure/Adventure-Plus-xlarge-dog.jpg' where id = '9';
+
+update product set imagename = 'adventure/Adventure-Plus-small-cat.jpg' where id = '1';
+update product set imagename = 'adventure/Adventure-Plus-medium-cat.jpg' where id = '2';
+update product set imagename = 'adventure/Adventure-Plus-large-cat.jpg' where id = '3';
+
+update product set imagename = 'shieldtec/ShieldTec-Plus-small-dog.jpg' where id = '14';
+update product set imagename = 'shieldtec/ShieldTec-Plus-medium-dog.jpg' where id = '15';
+update product set imagename = 'shieldtec/ShieldTec-Plus-large-dog.jpg' where id = '16';
+update product set imagename = 'shieldtec/ShieldTec-Plus-xlarge-dog.jpg' where id = '17';
+
+update product set imagename = 'frontline/Frontline-Plus-small-dog.jpg' where id = '18'; 
+update product set imagename = 'frontline/Frontline-Plus-medium-dog.jpg' where id = '19'; 
+update product set imagename = 'frontline/Frontline-Plus-large-dog.jpg' where id = '20'; 
+update product set imagename = 'frontline/Frontline-Plus-xlarge-dog.jpg' where id = '21'; 
+
+update product set imagename = 'frontline/Frontline-Plus-cat.jpg' where id = '25';
+update product set imagename = 'frontline/Frontline-Plus-cat.jpg' where id = '22';
+update product set imagename = 'frontline/Frontline-Plus-cat.jpg' where id = '24';
+
+
+dev:
+
+update product set imagename = 'zoguard/ZoGuard-Plus-small-dog.jpg' where id = '10';
+update product set imagename = 'zoguard/ZoGuard-Plus-medium-dog.jpg' where id = '11';
+update product set imagename = 'zoguard/ZoGuard-Plus-large-dog.jpg' where id = '12';
+update product set imagename = 'zoguard/ZoGuard-Plus-xlarge-dog.jpg' where id = '13';
+
+update product set imagename = 'zoguard/ZoGuard-Plus-cat-2.jpg' where id = '1';
+update product set imagename = 'zoguard/ZoGuard-Plus-cat-2.jpg' where id = '4';
+update product set imagename = 'zoguard/ZoGuard-Plus-cat-2.jpg' where id = '5';
+
+update product set imagename = 'adventure/Adventure-Plus-small-dog.jpg' where id = '6';
+update product set imagename = 'adventure/Adventure-Plus-medium-dog.jpg' where id = '7';
+update product set imagename = 'adventure/Adventure-Plus-large-dog.jpg' where id = '8';
+update product set imagename = 'adventure/Adventure-Plus-xlarge-dog.jpg' where id = '9';
+
+update product set imagename = 'adventure/Adventure-Plus-medium-cat.jpg' where id = '2';
+update product set imagename = 'adventure/Adventure-Plus-large-cat.jpg' where id = '3';
+
+update product set imagename = 'shieldtec/ShieldTec-Plus-small-dog.jpg' where id = '14';
+update product set imagename = 'shieldtec/ShieldTec-Plus-medium-dog.jpg' where id = '15';
+update product set imagename = 'shieldtec/ShieldTec-Plus-large-dog.jpg' where id = '16';
+update product set imagename = 'shieldtec/ShieldTec-Plus-xlarge-dog.jpg' where id = '17';
+
+update product set imagename = 'frontline/Frontline-Plus-small-dog.jpg' where id = '18'; 
+update product set imagename = 'frontline/Frontline-Plus-medium-dog.jpg' where id = '19'; 
+update product set imagename = 'frontline/Frontline-Plus-large-dog.jpg' where id = '20'; 
+update product set imagename = 'frontline/Frontline-Plus-xlarge-dog.jpg' where id = '21'; 
+
+update product set imagename = 'frontline/Frontline-Plus-cat.jpg' where id = '22';
+update product set imagename = 'frontline/Frontline-Plus-cat.jpg' where id = '23';
