ed65813a9304c20c885c4c2b1b1d3bdfdb49ce25
Mike Rivera <rivera.mj@gmail.com>
2018-02-11 23:17:38 -0500

    only send emails out in production2

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -55,7 +55,7 @@ class Boot {
     DataLoader.loadAdmin
     DataLoader.loadGroupons
     DataLoader.updateParentNoPets
-    //SpecialEmailSender.send5kEmails
+    SpecialEmailSender.send5kEmails
     
     // where to search snippet
     LiftRules.addToPackages("com.mypetdefense")
