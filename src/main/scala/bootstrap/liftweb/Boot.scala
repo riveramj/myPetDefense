cbc574c61664f9926b3a580e5c71aac9b544a4a1
Mike Rivera <rivera.mj@gmail.com>
2018-05-07 20:04:20 -0400

    remove test loader

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -57,7 +57,6 @@ class Boot {
     DataLoader.loadProducts
     DataLoader.loadAdmin
     DataLoader.updateParentNoPets
-    DataLoader.createGrowthRateTest
     
     // where to search snippet
     LiftRules.addToPackages("com.mypetdefense")
