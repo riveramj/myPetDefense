5df3f4e0f2ad1ef9332c74d666b1094db337c09a
Mike Rivera <rivera.mj@gmail.com>
2018-05-14 01:23:22 -0400

    one more attempt at dates

--- src/main/scala/com/mypetdefense/service/ShipmentService.scala
+++ src/main/scala/com/mypetdefense/service/ShipmentService.scala
@@ -12,7 +12,7 @@ object ShipmentService extends Loggable {
   def getCurrentPastDueShipments = {
     Shipment.findAll(
       BySql(
-        "expectedShipDate < current_date and expectedShipDate > current_date - interval '15 day'",
+        "expectedShipDate < current_date + interval '23 hour' + interval '55 minutes' and expectedShipDate > current_date - interval '15 day'",
         IHaveValidatedThisSQL("mike","2018-04-24")
       ),
       NullRef(Shipment.dateShipped),
