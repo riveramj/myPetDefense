27c329e58095f9def7956134d9750127a1e0a335
Mike Rivera <rivera.mj@gmail.com>
2018-05-31 23:42:46 -0400

    11:47pm email

--- src/main/scala/com/mypetdefense/jobs/EmailReportingJob.scala
+++ src/main/scala/com/mypetdefense/jobs/EmailReportingJob.scala
@@ -34,7 +34,7 @@ object DailySalesReportEmailJob extends TriggeredJob {
   val trigger = TriggerBuilder.newTrigger()
     .withIdentity("DailySalesReportEmailJobTrigger")
     .startNow()
-    .withSchedule(CronScheduleBuilder.cronSchedule("0 0 23 ? * * *"))
+    .withSchedule(CronScheduleBuilder.cronSchedule("0 47 23 ? * * *"))
     .build()
 }
 
