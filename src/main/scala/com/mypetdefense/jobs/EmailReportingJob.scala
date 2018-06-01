747b613a2c0c3c47d4f5aac9819207afa7a8417b
Mike Rivera <rivera.mj@gmail.com>
2018-05-31 23:48:01 -0400

    bump time for email 2

--- src/main/scala/com/mypetdefense/jobs/EmailReportingJob.scala
+++ src/main/scala/com/mypetdefense/jobs/EmailReportingJob.scala
@@ -34,7 +34,7 @@ object DailySalesReportEmailJob extends TriggeredJob {
   val trigger = TriggerBuilder.newTrigger()
     .withIdentity("DailySalesReportEmailJobTrigger")
     .startNow()
-    .withSchedule(CronScheduleBuilder.cronSchedule("0 49 23 ? * * *"))
+    .withSchedule(CronScheduleBuilder.cronSchedule("0 55 23 ? * * *"))
     .build()
 }
 
