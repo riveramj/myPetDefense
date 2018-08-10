84abade1462260aa3d382cfcdc4ee41463cbf8ca
Mike Rivera <rivera.mj@gmail.com>
2018-08-10 18:10:43 +0200

    get Shipstation Order by Id

---
+++ src/main/scala/com/mypetdefense/service/ShipStationService.scala
@@ -0,0 +1,66 @@
+package com.mypetdefense.service
+
+import net.liftweb._ 
+  import common._
+  import util.Helpers.tryo
+  import json._
+  import util.Props
+
+import dispatch._, Defaults._
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+import scala.collection.concurrent.TrieMap
+import scala.util.{Failure => TryFail, _}
+import scala.math.BigDecimal
+
+import java.util.Date
+import java.util.Base64
+import java.nio.charset.StandardCharsets
+
+object ShipStationService extends Loggable {
+  val baseShipStationUrl = Props.get("shipstation.url") openOr ""
+  val shipStationKey = Props.get("shipstation.key") openOr ""
+  val shipStationSecret = Props.get("shipstation.secret") openOr ""
+
+  val retryAttempts = 10
+
+  val basicAuthentication = Base64.getEncoder.encodeToString(s"${shipStationKey}:${shipStationSecret}".getBytes(StandardCharsets.UTF_8))
+
+  println(basicAuthentication)
+
+  def getOrder(orderId: String) = {
+    val orderUrl = url(s"${baseShipStationUrl}/orders/${orderId}")
+
+    def getOrderResponse = {
+      Http(orderUrl <:< Map("Authorization" -> s"Basic ${basicAuthentication}") OK as.String).either.map {
+        case Left(throwable) =>
+          logger.error(s"error from ShipStation: ${throwable}")
+          Failure("Error occured while talking to shipstation.", Full(throwable), Empty)
+        case Right(possibleOrder) =>
+          Full(possibleOrder)
+      }
+    }
+
+    def rawOrder(attemptsLeft: Int): Box[String] = {
+      Try(Await.result(getOrderResponse, 1 seconds)) match {
+        case Success(orderResponse) => 
+          orderResponse
+        case TryFail(throwable: Throwable) =>
+          if (attemptsLeft > 0)
+            rawOrder(attemptsLeft - 1)
+          else {
+            logger.error(s"Timeout occured while talking to shipStation for get order with ${throwable}")
+            Failure("Timeout occured while talking to shipStation for get order.", Full(throwable), Empty)
+          }
+
+      }
+    }
+
+    val order = rawOrder(retryAttempts)
+
+    println(order)
+    order
+  }
+}
