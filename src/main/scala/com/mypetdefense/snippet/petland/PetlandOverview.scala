d7f04b5c8322ceb2f197246836a3abf369bd3d92
Mike Rivera <rivera.mj@gmail.com>
2018-12-10 16:27:31 -0500

    use 2 per sale

--- src/main/scala/com/mypetdefense/snippet/petland/PetlandOverview.scala
+++ src/main/scala/com/mypetdefense/snippet/petland/PetlandOverview.scala
@@ -189,8 +189,8 @@ class PetlandOverview extends Loggable {
       }
     }
 
-    val ytdCommission = currentYearSignups.size * 2
-    val mtdCommission = currentMonthSignups.size * 2
+    val ytdCommission = currentYearSignups.size * 12
+    val mtdCommission = currentMonthSignups.size * 12
 
     ".mtd-shipments *" #> currentMonthSubscriptionShipments.size &
     ".mtd-commission-earned *" #> s"$$$mtdCommission" &
