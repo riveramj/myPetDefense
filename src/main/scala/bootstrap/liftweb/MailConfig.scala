7059b5cb1426da95dae278f07302d4ed61dd2081
Mike Rivera <rivera.mj@gmail.com>
2020-02-05 23:20:43 -0500

    mailer take 1

--- src/main/scala/bootstrap/liftweb/MailConfig.scala
+++ src/main/scala/bootstrap/liftweb/MailConfig.scala
@@ -12,6 +12,8 @@ object MailConfig extends Loggable {
     if (Props.mode == Props.RunModes.Development) {
       Mailer.devModeSend.default.set((m: MimeMessage) => logger.info("Dev mode message:\n" + prettyPrintMime(m)))
     } else {
+      Mailer.customProperties = Map("mail.smtp.starttls.enable" -> "true")
+
       (Props.get("mail.user"), Props.get("mail.password")) match {
         case (Full(username), Full(password)) =>
         Mailer.authenticator = Full(new Authenticator() {
