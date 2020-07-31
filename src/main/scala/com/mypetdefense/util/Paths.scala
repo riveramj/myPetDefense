224e954cc50fda9bbea31669b2dc870103e6228f
Mike Rivera <rivera.mj@gmail.com>
2020-07-31 07:39:19 -0400

    almost 2

--- src/main/scala/com/mypetdefense/util/Paths.scala
+++ src/main/scala/com/mypetdefense/util/Paths.scala
@@ -52,7 +52,7 @@ object Paths {
     RedirectResponse(Login.menu.loc.calcDefaultHref)
   }
 
-  val homePage = Menu.i("Home") / "index" >> RedirectResponse("mypetdefense.com")
+  val homePage = Menu.i("Home") / "index"
   val termsOfService = Menu.i("Terms of Service") / "terms-of-service"
   
   val thanksPage = Menu.i("Thanks") / "thanks"
