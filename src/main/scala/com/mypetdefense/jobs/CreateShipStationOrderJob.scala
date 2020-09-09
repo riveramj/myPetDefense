767c74ebaa9533e948c00fb9a31307588c292a89
Mike Rivera <rivera.mj@gmail.com>
2020-09-08 23:02:44 -0500

    try again

--- src/main/scala/com/mypetdefense/jobs/CreateShipStationOrderJob.scala
+++ src/main/scala/com/mypetdefense/jobs/CreateShipStationOrderJob.scala
@@ -27,27 +27,35 @@ class CreateShipStationOrderJob extends ManagedJob {
     )
 
     println(newShipments.size + " shipment size")
+    var count = 0
 
     for {
       shipment <- newShipments
+      _ = println(count)
       subscription <- shipment.subscription.obj
+      _ = println(count)
       user <- subscription.user.obj
-    } yield { 
+      _ = println(count)
+    } yield {
+
       val shipStationOrder = ShipStationService.createShipStationOrder(shipment, user, subscription)
 
       shipStationOrder.onComplete {
         case TrySuccess(Full(order)) =>
-          shipment.shipStationOrderId(order.orderId).shipmentStatus(ShipmentStatus.LabelCreated).saveMe
+          println(count + " in success")
+          shipment.refresh.map(_.shipStationOrderId(order.orderId).shipmentStatus(ShipmentStatus.LabelCreated).saveMe)
 
           if (!sameDateComparison(
             new Date(),
             shipment.expectedShipDate.get
           )) {
+            println(count + " in compare")
             ShipStationService.holdOrderUntil(
               order.orderId,
               shipment.expectedShipDate.get
             ).onComplete {
               case TrySuccess(Full(_)) =>
+                println(count + " in compare success")
 
               case TrySuccess(shipStationFailure) =>
                 logger.error(s"hold order failed with shipStation error: ${shipStationFailure}")
@@ -68,6 +76,7 @@ class CreateShipStationOrderJob extends ManagedJob {
             logger.error(s"user email is ${user.email.get}")
             throwable
       }
+      count = count + 1
     }
   }
 }
