7712e7d2c4d2cd08f7a4b3374bc3fca09b443d7f
bartosso <barton547@gmail.com>
2020-10-20 18:02:09 +0800

    cats for some

--- src/test/scala/com/mypetdefense/service/CheckoutServiceSpec.scala
+++ src/test/scala/com/mypetdefense/service/CheckoutServiceSpec.scala
@@ -5,6 +5,7 @@ import com.mypetdefense.helpers.BootUtil
 import com.mypetdefense.helpers.GeneralDbUtils.clearTables
 import com.mypetdefense.model.{Address, Pet, Price, Status, Subscription, User}
 import com.mypetdefense.snippet.signup.NewUserAddress
+import cats.syntax.option._
 import net.liftweb.common.{Box, Empty}
 import org.scalatest.{Assertion, BeforeAndAfterAll, BeforeAndAfterEach}
 import org.scalatest.flatspec.AnyFlatSpec
@@ -35,11 +36,11 @@ class CheckoutServiceSpec
       maybeAddress: Option[Address],
       expectedAddress: NewUserAddress
   ): Assertion = {
-    maybeAddress.map(_.street1.get.toLowerCase) shouldBe Some(expectedAddress.street1.toLowerCase)
-    maybeAddress.map(_.street2.get.toLowerCase) shouldBe Some(expectedAddress.street2.toLowerCase)
-    maybeAddress.map(_.city.get.toLowerCase) shouldBe Some(expectedAddress.city.toLowerCase)
-    maybeAddress.map(_.state.get.toLowerCase) shouldBe Some(expectedAddress.state.toLowerCase)
-    maybeAddress.map(_.zip.get.toLowerCase) shouldBe Some(expectedAddress.zip.toLowerCase)
+    maybeAddress.map(_.street1.get.toLowerCase) shouldBe expectedAddress.street1.toLowerCase.some
+    maybeAddress.map(_.street2.get.toLowerCase) shouldBe expectedAddress.street2.toLowerCase.some
+    maybeAddress.map(_.city.get.toLowerCase) shouldBe expectedAddress.city.toLowerCase.some
+    maybeAddress.map(_.state.get.toLowerCase) shouldBe expectedAddress.state.toLowerCase.some
+    maybeAddress.map(_.zip.get.toLowerCase) shouldBe expectedAddress.zip.toLowerCase.some
   }
 
   it should "create user, subscription, address and pet on setting up new user" in {
@@ -62,7 +63,7 @@ class CheckoutServiceSpec
     val maybeAddress = Address.findAll().find(_.user.get == createdUser.id.get)
 
     sub.isDefined shouldBe true
-    sub.map(_.status.get) shouldBe Some(Status.Active)
+    sub.map(_.status.get) shouldBe Status.Active.some
     pets.size shouldBe petsToCreate.size
     compareAddressWithExpected(maybeAddress, generatedUserData.address)
   }
