d276d0ed5235d0c54dc16e084d670bb816d804aa
Mike Rivera <rivera.mj@gmail.com>
2020-12-10 23:54:46 -0500

    run on 59

--- src/main/scala/com/mypetdefense/jobs/SubscriptionBoxProductsUpdateJob.scala
+++ src/main/scala/com/mypetdefense/jobs/SubscriptionBoxProductsUpdateJob.scala
@@ -90,7 +90,7 @@ object DailySubscriptionBoxProductsUpdateJob extends TriggeredJob {
     .newTrigger()
     .withIdentity("DailySubscriptionBoxProductsUpdateJobTrigger")
     .startNow()
-    .withSchedule(CronScheduleBuilder.cronSchedule("0 55 0 ? * * *"))
+    .withSchedule(CronScheduleBuilder.cronSchedule("0 0 0 ? * * *"))
     .build()
 }
 
