c2f2ddaa09d4769b9c672a1cd2d8e0b563b51853
Mike Rivera <rivera.mj@gmail.com>
2021-01-14 13:51:14 -0500

    rename stripe price name

--- src/main/scala/com/mypetdefense/constants/StripePrices.scala
+++ src/main/scala/com/mypetdefense/constants/StripePrices.scala
@@ -6,24 +6,24 @@ object StripePrices {
   final case class ProductBox(priceId: String, monthlyCharge: BigDecimal)
 
   object Cat {
-    val BasicBox: ProductBox = ProductBox(`stripe.prices.boxes.cat.basic.id`, BigDecimal("12.99"))
+    val BasicBox: ProductBox = ProductBox(stripePricesBoxesCatBasicId, BigDecimal("12.99"))
   }
 
   object Dog {
-    val BasicBox: ProductBox = ProductBox(`stripe.prices.boxes.dog.basic.id`, BigDecimal("12.99"))
+    val BasicBox: ProductBox = ProductBox(stripePricesBoxesDogBasicId, BigDecimal("12.99"))
 
     object HealthAndWellnessBox {
-      val Small: ProductBox  = ProductBox(`stripe.prices.boxes.dog.hwsmall.id`, BigDecimal("24.99"))
-      val Medium: ProductBox = ProductBox(`stripe.prices.boxes.dog.hwmedxl.id`, BigDecimal("27.99"))
+      val Small: ProductBox  = ProductBox(stripePricesBoxesDogHwSmallId, BigDecimal("24.99"))
+      val Medium: ProductBox = ProductBox(stripePricesBoxesDogHwMedXlId, BigDecimal("27.99"))
       val Large: ProductBox  = Medium
       val XLarge: ProductBox = Medium
     }
   }
 
-  private val `stripe.prices.boxes.cat.basic.id`   = priceId("stripe.prices.boxes.cat.basic.id")
-  private val `stripe.prices.boxes.dog.basic.id`   = priceId("stripe.prices.boxes.dog.basic.id")
-  private val `stripe.prices.boxes.dog.hwsmall.id` = priceId("stripe.prices.boxes.dog.hwsmall.id")
-  private val `stripe.prices.boxes.dog.hwmedxl.id` = priceId("stripe.prices.boxes.dog.hwmedxl.id")
+  private val stripePricesBoxesCatBasicId   = priceId("stripe.prices.boxes.cat.basic.id")
+  private val stripePricesBoxesDogBasicId   = priceId("stripe.prices.boxes.dog.basic.id")
+  private val stripePricesBoxesDogHwSmallId = priceId("stripe.prices.boxes.dog.hw.small.id")
+  private val stripePricesBoxesDogHwMedXlId = priceId("stripe.prices.boxes.dog.hw.med.xl.id")
 
   private def priceId(name: String): String =
     Props.get(name).openOrThrowException(s"Price ID '$name' is required")
