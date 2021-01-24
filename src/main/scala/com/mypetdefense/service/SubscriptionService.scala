5ce5c1bded5bc4ac29d1fb4416311bc3e8e1a616
Mike Rivera <rivera.mj@gmail.com>
2021-01-24 08:58:08 -0700

    upgrade account in the admin panel

--- src/main/scala/com/mypetdefense/service/SubscriptionService.scala
+++ src/main/scala/com/mypetdefense/service/SubscriptionService.scala
@@ -1,8 +1,15 @@
 package com.mypetdefense.service
 
-import java.time.Month
+import com.mypetdefense.actor.{EmailActor, UpgradeSubscriptionEmail}
+import com.mypetdefense.model._
+import com.mypetdefense.service.ParentService.updateStripeSubscriptionQuantity
+import com.mypetdefense.util.SecurityContext
+import net.liftweb.common.Box
+import net.liftweb.http.js.JsCmds.Alert
+import net.liftweb.util.Helpers.tryo
+import net.liftweb.util.Props
 
-import com.mypetdefense.model.{Status, Subscription}
+import java.time.Month
 
 object SubscriptionService {
 
@@ -22,4 +29,36 @@ object SubscriptionService {
     }
   }
 
+  def upgradeAccount(updatedSubscription: Box[Subscription], admin: Boolean = false) = {
+    val boxes = updatedSubscription.map(_.subscriptionBoxes.toList).openOr(Nil)
+
+    boxes.map(SubscriptionItem.createFirstBox)
+    val updatedBoxes = boxes.map(_.reload)
+
+    val cost = updatedBoxes.map(SubscriptionBox.possiblePrice(_, true)).sum
+
+    updateStripeSubscriptionQuantity(
+      updatedSubscription.map(_.stripeSubscriptionId.get).openOr(""),
+      tryo((cost * 100).toInt).openOr(0)
+    )
+
+    if (Props.mode == Props.RunModes.Production) {
+      EmailActor ! UpgradeSubscriptionEmail(SecurityContext.currentUser, updatedBoxes.size)
+    }
+
+    for {
+      box          <- updatedBoxes
+      subscription <- updatedSubscription.toList
+      user         <- SecurityContext.currentUser.toList
+      shipmentCount = subscription.shipments.toList.size
+    } yield {
+      SubscriptionUpgrade.createSubscriptionUpgrade(subscription, box, user, shipmentCount)
+      subscription.isUpgraded(true).saveMe()
+    }
+
+    if (admin)
+      Alert("Account has been upgraded.")
+    else
+      Alert("Your account has been upgraded! Watch the mail for your new box!")
+  }
 }
