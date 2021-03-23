6cae6016edd8e3a84ec81c6b11fd5db24b076ee1
Mike Rivera <rivera.mj@gmail.com>
2021-03-23 12:02:57 -0500

    actually use emails

--- src/main/scala/com/mypetdefense/jobs/SalesAgentEmailReportingJob.scala
+++ src/main/scala/com/mypetdefense/jobs/SalesAgentEmailReportingJob.scala
@@ -1,7 +1,9 @@
 package com.mypetdefense.jobs
 
 import com.mypetdefense.actor._
+import com.mypetdefense.model.{EmailReport, ReportType}
 import com.mypetdefense.service.ReportingService
+import net.liftweb.mapper.By
 import org.quartz._
 
 class SalesAgentReportEmailJob extends ManagedJob {
@@ -13,8 +15,9 @@ class SalesAgentReportEmailJob extends ManagedJob {
     val dailyAgencyData   = ReportingService.findYesterdaySalesByAgency
     val monthlyAgencyData = ReportingService.findMTDSalesByAgency
 
-    val emails = EmailReport.findAll(By(EmailReport.reportType, ReportType.DailyTPPAgentSalesReportEmail))
-      
+    val report = EmailReport.findAll(By(EmailReport.reportType, ReportType.DailyTPPAgentSalesReportEmail))
+    val emails = report.flatMap(_.emailRecords.toList).map(_.email.get)
+
     emails.map { email =>
       EmailActor ! DailySalesEmail(
         dailyAgentData,
