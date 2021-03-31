872ff125b50d785dd1d69cb470484098c8ccfe27
Mike Rivera <rivera.mj@gmail.com>
2021-03-30 21:49:41 -0500

    turn off freq job

--- src/main/scala/com/mypetdefense/jobs/JobsManager.scala
+++ src/main/scala/com/mypetdefense/jobs/JobsManager.scala
@@ -56,7 +56,7 @@ object JobManager extends SimpleInjector with Loggable {
     //FrequentTrackShipmentDeliveryJob ::
     //FrequentCreateTreatLabelJob ::
     //FrequentSubscriptionBoxProductsUpdateJob ::
-    FrequentRecordStatisticsSnapshotJob ::
+    //FrequentRecordStatisticsSnapshotJob ::
     Nil
   }
 }
