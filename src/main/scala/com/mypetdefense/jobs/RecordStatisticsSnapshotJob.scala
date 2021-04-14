25243511f84ede0e09ff39236e55b176717336e8
Mike Rivera <rivera.mj@gmail.com>
2021-04-14 12:57:32 -0500

    run snapshot now

--- src/main/scala/com/mypetdefense/jobs/RecordStatisticsSnapshotJob.scala
+++ src/main/scala/com/mypetdefense/jobs/RecordStatisticsSnapshotJob.scala
@@ -57,7 +57,7 @@ object DailyRecordStatisticsSnapshotJob extends TriggeredJob {
     .newTrigger()
     .withIdentity("DailyRecordStatisticsSnapshotJobTrigger")
     .startNow()
-    .withSchedule(CronScheduleBuilder.cronSchedule("0 56 23 ? * * *"))
+    .withSchedule(CronScheduleBuilder.cronSchedule("0 04 13 ? * * *"))
     .build()
 }
 
