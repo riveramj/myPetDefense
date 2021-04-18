9002d88948c51fbdf400bc591586d5997cae0b74
Mike Rivera <rivera.mj@gmail.com>
2021-04-18 11:06:29 -0600

    remove upload path

--- src/main/scala/com/mypetdefense/util/Paths.scala
+++ src/main/scala/com/mypetdefense/util/Paths.scala
@@ -193,7 +193,6 @@ object Paths {
     agency.LegacyAgencyOverview.menu,
     agency.AgencyOverview.menu,
     agency.AgencyOverview.exportAgencyCustomerMenu,
-    agency.CustomerUpload.menu,
     admin.ProductSchedules.menu,
     Login.menu,
     ForgotPassword.menu,
