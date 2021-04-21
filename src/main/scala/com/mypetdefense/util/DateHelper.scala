3ffa74d8d724d4cda78349489495614d1976acce
Mike Rivera <rivera.mj@gmail.com>
2021-04-20 18:16:09 -0600

    null dates

--- src/main/scala/com/mypetdefense/util/DateHelper.scala
+++ src/main/scala/com/mypetdefense/util/DateHelper.scala
@@ -1,15 +1,14 @@
 package com.mypetdefense.util
 
 import net.liftweb.common.Box
+import net.liftweb.util.Helpers.tryo
 
 import java.text.SimpleDateFormat
 import java.time._
 import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
 import java.time.temporal.ChronoUnit.MILLIS
 import java.util.{Date, Locale}
-import net.liftweb.util.Helpers.tryo
-
-import java.time.temporal.ChronoUnit
 
 object DateHelper {
   val dateFormat     = new SimpleDateFormat("MM/dd/yyyy")
@@ -106,7 +105,11 @@ object DateHelper {
 
   def getMonthDiff(dateOne: Date, dateTwo: Box[Date]): Long = {
     val startDate = dateOne.toLocalDate
-    val endDate = dateTwo.openOr(nowDate).toLocalDate
+    val endDate =
+      if (dateTwo.contains(null))
+        now
+      else
+        dateTwo.openOr(nowDate).toLocalDate
 
     ChronoUnit.MONTHS.between(startDate, endDate)
   }
